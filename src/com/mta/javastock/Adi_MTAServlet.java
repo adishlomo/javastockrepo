30da24c2c7283f5ed943ef134b9da31ceba0f541
adi <adi@adi-PC>
2015-04-01 14:57:49 +0300

    ex2 complete

--- src/com/mta/javastock/Adi_MTAServlet.java
+++ src/com/mta/javastock/Adi_MTAServlet.java
@@ -4,8 +4,17 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class Adi_MTAServlet extends HttpServlet {
+	
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1=3, num2=4, num3=7;
+		int result=(num1+num2)*num3;
+		
+
+		String resultStr = new String("<h1>Result of: (" +num1+ "+" +num2+ ")*" +num3+ "=" +result+"</h1>");
+				
+		resp.getWriter().println(resultStr);
+	
 	}
 }
